38d5b722d23aef774fbff1dcd37af9ad6b2ac403
shiti Franky <frankyshiti737@gmail.com>
2025-07-01 17:43:33 +0100

    besafe services app

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const app = require('./app');
 
 //locale
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
 
 //production
