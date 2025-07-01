5933e8c428cb8b41984ff63b95efe3e8b17e02d5
shiti Franky <frankyshiti737@gmail.com>
2025-07-01 18:04:02 +0100

    change port service besafe

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = require('./app');
 const server = http.createServer(app); */
 
 //production
-const PORT = process.env.PORT || 443;
+const PORT = process.env.PORT || 8443;
 
 const options = {
   key: fs.readFileSync('./ssl/camtrack_net.key'),
